c03789ccb87631ae88f4d4431a22230ca3324194
Svetik <31988884+Svetik3d@users.noreply.github.com>
2017-10-24 20:47:52 +0400

    Update json.py

--- json.py
+++ json.py
@@ -7,20 +7,23 @@ import requests
 import datetime
 
 URL = 'https://api.sunrise-sunset.org/json?lat=55.729887&lng=38.941911&date=today&formatted=1'
+date = 0
 
-def sunsetfunkcia():
-	result = requests.get(URL).json()["results"]["sunset"]
-	sunset = result.split(":")
-	sunseth = int(sunset[0])
-	sunsetm = int(sunset[1])
-	nowt = datetime.datetime.now()
-	nowh = nowt.hour+13
-	if nowh<0:
-		nowh=nowh-24
-	nowm = nowt.minute
-	if nowh >= sunseth and sunsetm >= nowm and nowm < sunsetm+5:
+def sunsetfunkcia(nowh = datetime.datetime.now().hour, 	nowm = datetime.datetime.now().minute):
+	if date != datetime.date.day:
+		date = datetime.date.day
+		result = requests.get(URL).json()["results"]["sunset"]
+		sunset = result.split(":")
+		sunseth = int(sunset[0])+13
+		sunsetm = int(sunset[1])
+		print 'СХОДИЛ ЗА ДАТОЙ'
+	if sunseth>24:
+		sunseth=sunseth-24
+	if nowh == sunseth and sunsetm >= nowm and nowm < sunsetm+5:
 		return "ON"
 	if nowh >=20 and nowm>=5:
 		return "OFF"
 
-print sunsetfunkcia()
+for h in range(24):
+	for m in range(0,60,5):
+		print sunsetfunkcia(h,m)
