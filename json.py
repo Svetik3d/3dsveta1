83106e513567de1a376bbd4934b447afb3c4e79d
Svetik <31988884+Svetik3d@users.noreply.github.com>
2017-10-29 22:47:37 +0400

    Update json.py

--- json.py
+++ json.py
@@ -9,18 +9,18 @@ import os
 
 URL = 'https://api.sunrise-sunset.org/json?lat=55.729887&lng=38.941911&date=today&formatted=1'
 uts = 1
+def settime():
+	with open("/tmp/saved_time", "w") as files:
+		d  = datetime.datetime.now().day
+		result = requests.get(URL).json()["results"]["sunset"]
+		sunset = result.split(":")
+		sunseth = int(sunset[0])+12+uts
+		sunsetm = int(sunset[1])
+		sumarno = str(d) +":"+ str(sunseth)+":"+str(sunsetm)
+		files.write(sumarno)
+		return sunseth, sunsetm
 
-def sunsetfunkcia(nowh = datetime.datetime.now().hour, 	nowm = datetime.datetime.now().minute):
-	def settime():
-		with open("/tmp/saved_time", "w") as files:
-			d  = datetime.datetime.now().day
-			result = requests.get(URL).json()["results"]["sunset"]
-			sunset = result.split(":")
-			sunseth = int(sunset[0])+12+uts
-			sunsetm = int(sunset[1])
-			sumarno = str(d) +":"+ str(sunseth)+":"+str(sunsetm)
-			files.write(sumarno)
-			return sunseth, sunsetm
+def sunsetfunkcia(nowh = datetime.datetime.now().hour, nowm = datetime.datetime.now().minute):
 	nalf = os.path.exists("/tmp/saved_time")
 	if nalf == False:
 		sunseth, sunsetm = settime()
@@ -34,15 +34,15 @@ def sunsetfunkcia(nowh = datetime.datetime.now().hour, 	nowm = datetime.datetime
 				sunsetm = int(w[2])
 			else:
 				sunseth, sunsetm = settime()
-	if sunseth>24:
-		sunseth=sunseth-24
+	if sunseth > 24:
+		sunseth = sunseth - 24
 	if sunseth == nowh  and sunsetm <= nowm:
 		return "ON"
 	if sunseth < nowh < 20:
 		return "ON"
-	if nowh >=20 or nowh<sunseth:
+	if nowh >= 20 or nowh < sunseth:
 		return "OFF"
-	if nowh == sunseth or nowm<sunsetm:
+	if nowh == sunseth or nowm < sunsetm:
 		return "OFF"
 
 for h in range(24):
