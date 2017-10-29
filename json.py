67c8b9dab42e473e0d7a412a0ae27788e96461bf
Svetik <31988884+Svetik3d@users.noreply.github.com>
2017-10-29 20:26:58 +0400

    Update json.py

--- json.py
+++ json.py
@@ -5,16 +5,38 @@
 import json
 import requests
 import datetime
+import os
 
 URL = 'https://api.sunrise-sunset.org/json?lat=55.729887&lng=38.941911&date=today&formatted=1'
 uts = 1
 
-
 def sunsetfunkcia(nowh = datetime.datetime.now().hour, 	nowm = datetime.datetime.now().minute):
-	result = requests.get(URL).json()["results"]["sunset"]
-	sunset = result.split(":")
-	sunseth = int(sunset[0])+12+uts
-	sunsetm = int(sunset[1])
+	nalf = os.path.exists("/tmp/saved_time")
+	if nalf == False:
+		with open("/tmp/saved_time", "w") as files:
+			d  = datetime.datetime.now().day
+			result = requests.get(URL).json()["results"]["sunset"]
+			sunset = result.split(":")
+			sunseth = int(sunset[0])+12+uts
+			sunsetm = int(sunset[1])
+			sumarno = str(d) +":"+ str(sunseth)+":"+str(sunsetm)
+			files.write(sumarno)
+	else:
+		with open("/tmp/saved_time", "r") as files:
+			d  = datetime.datetime.now().day
+			w = files.read().split(":")
+			date = w[0]
+			if date == str(d):
+				sunseth = int(w[1])
+				sunsetm = int(w[2])
+			else:
+				with open("/tmp/saved_time", "w") as files:
+					result = requests.get(URL).json()["results"]["sunset"]
+					sunset = result.split(":")
+					sunseth = int(sunset[0])+12+uts
+					sunsetm = int(sunset[1])
+					sumarno = str(d) +":"+ str(sunseth)+":"+str(sunsetm)
+					files.write(sumarno)
 	if sunseth>24:
 		sunseth=sunseth-24
 	if sunseth <= nowh < 20:
@@ -22,7 +44,7 @@ def sunsetfunkcia(nowh = datetime.datetime.now().hour, 	nowm = datetime.datetime
 	if nowh >=20 or nowh<sunseth:
 		return "OFF"
 
-for h in range(24):
+for h in range(12,24):
 	for m in range(0,60,5):
 		print str(h)+":"+str(m)
 		print sunsetfunkcia(h,m)
